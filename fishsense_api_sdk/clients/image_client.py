3657f5c9ef89e93d2f92e2cf3df1504e43cab192
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-10-14 18:00:54 -0700

    fix: missing model class and api implementation

--- fishsense_api_sdk/clients/image_client.py
+++ fishsense_api_sdk/clients/image_client.py
@@ -9,3 +9,14 @@ from fishsense_api_sdk.models.image import Image
 class ImageClient(ClientBase):
     def __init__(self, base_url: str):
         super().__init__(base_url)
+
+    async def get(self, dive_id: int | None = None) -> Image | List[Image]:
+        async with httpx.AsyncClient() as client:
+            if dive_id is not None:
+                response = await client.get(
+                    f"{self.base_url}/api/v1/dives/{dive_id}/images/"
+                )
+                response.raise_for_status()
+                return Image.model_validate(response.json())
+            else:
+                raise NotImplementedError("Getting all images is not supported.")
--- fishsense_api_sdk/models/image.py
+++ fishsense_api_sdk/models/image.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class Dive(BaseModel):
+class Image(BaseModel):
     """Model representing an image."""
 
     id: int | None
