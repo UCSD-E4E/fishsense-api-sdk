f1da2797a0b73b991da1431d10d748f2c8d8dc3d
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-10-14 20:41:17 -0700

    feat: introduce get specific image by id

--- fishsense_api_sdk/clients/image_client.py
+++ fishsense_api_sdk/clients/image_client.py
@@ -9,12 +9,18 @@ class ImageClient(ClientBase):
     def __init__(self, base_url: str, timeout: int):
         super().__init__(base_url, timeout)
 
-    async def get(self, dive_id: int | None = None) -> Image | List[Image]:
+    async def get(
+        self, dive_id: int | None = None, image_id: int | None = None
+    ) -> Image | List[Image]:
         async with self._create_client() as client:
             if dive_id is not None:
                 response = await client.get(f"/api/v1/dives/{dive_id}/images/")
                 response.raise_for_status()
                 return [Image.model_validate(image) for image in response.json()]
+            elif image_id is not None:
+                response = await client.get(f"/api/v1/images/{image_id}")
+                response.raise_for_status()
+                return Image.model_validate(response.json())
             else:
                 raise NotImplementedError("Getting all images is not supported.")
 
