b0ed74186731932947f80ac11553409e5620a72d
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-10-14 23:02:01 -0700

    support getting single camera

--- fishsense_api_sdk/clients/camera_client.py
+++ fishsense_api_sdk/clients/camera_client.py
@@ -12,11 +12,16 @@ class CameraClient(ClientBase):
     def __init__(self, base_url: str, timeout: int):
         super().__init__(base_url, timeout)
 
-    async def get(self) -> List[Camera]:
+    async def get(self, camera_id: int | None = None) -> List[Camera] | Camera:
         async with self._create_client() as client:
-            response = await client.get("/api/v1/cameras/")
-            response.raise_for_status()
-            return [Camera.model_validate(camera) for camera in response.json()]
+            if camera_id is not None:
+                response = await client.get(f"/api/v1/cameras/{camera_id}")
+                response.raise_for_status()
+                return Camera.model_validate(response.json())
+            else:
+                response = await client.get("/api/v1/cameras/")
+                response.raise_for_status()
+                return [Camera.model_validate(camera) for camera in response.json()]
 
     async def post_intrinsics(
         self, camera_id: int, camera_intrinsics: CameraIntrinsics
